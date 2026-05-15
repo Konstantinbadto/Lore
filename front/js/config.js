1c59c3abd9a0810128bd2704183f652bb6b8ddb3
BadTom <ajrih.k.i.23@gmail.com>
2026-05-15 12:21:12 +0300

    serve frontend from render12

--- front/js/config.js
+++ front/js/config.js
@@ -3,20 +3,16 @@ const API_CONFIG = {
     getBaseUrl: function() {
         const hostname = window.location.hostname;
 
-        // Локальная разработка
         if (hostname === "localhost" || hostname === "127.0.0.1") {
             return "http://localhost:3307";
         }
 
-        // Продакшен (Render)
         return "";
     }
 };
 
 // Инициализация
 API_CONFIG.API_BASE = API_CONFIG.getBaseUrl();
-
-// Делаем глобально доступным
 window.API_CONFIG = API_CONFIG;
 
 console.log('✅ API_CONFIG загружен →', API_CONFIG.API_BASE);
